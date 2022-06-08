e5ea60c09612a862b02d989fac7c459afec49b9f
Carl Lippert <carl.lippert@gmail.com>
2022-06-08 17:03:15 -0300

    Adding struct to describe v0 royalties system.

--- contracts/src/NFT.sol
+++ contracts/src/NFT.sol
@@ -14,8 +14,35 @@ contract NFT is ERC721, Ownable {
     using Strings for uint256;
     uint256 public currentTokenId;
 
+    struct Job {
+        //for the system, address, etc that mints the NFT
+        //economic incentive for integration into 3rd party user tools like Asana, Github, Airtable, etc
+        //maybe not store? would improve efficiency since payout is at mint
+        //may also be not regularly used. Platforms could say "pay for enterprise with USD" and they just cover txn costs and
+        //don't include a "creator fee" on the mint transactions they facilitate
+        uint256 creatorFee; 
+        //for the dao, guild, marketplace, human, contract etc that helps find the person who does the "work"
+        //this will often be kept 0 because no payment may be included since this is just part of a salaried job
+        //inspired by recruiter fee naming for zora asks
+        uint256 recruiterFee;
+        //for the entity doing the work. Built in way to hire 3rd parties native to the protocol
+        //also often left at zero. For this may be a salaried job not an external one. 
+        uint256 exectuterFee;
+        //in future allow dif currencies for everything
+        //at least at first blush that seems to be something people would want 
+        address jobCurrency;
+        //the date that something is claimable till. 
+        //expressed as a blockstamp
+        //unsure if this should actually exist
+        //external contract could be set up to use oracle to "burn" this job if it is not completed by date
+        uint256 deadline; 
+    }
+
     // Custom URI for each token
     mapping(uint256 => string) private _tokenURIs;
+
+    //royalties
+    mapping(uint256 => Job) private _jobs; 
 
     constructor(string memory _name, string memory _symbol)
         ERC721(_name, _symbol)
