4eb35a40eebc1e19e4e3be1301e04ff07ea18ea1
Carl Lippert <carl.lippert@gmail.com>
2022-06-10 13:26:22 -0300

    New contract created on local chain.

--- web/utils/consts.tsx
+++ web/utils/consts.tsx
@@ -1,4 +1,4 @@
-export const APP_NAME = "App Name";
-export const contract_address = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+export const APP_NAME = "MLS IOJ";
+export const contract_address = "0xdc64a140aa3e981100a9beca4e685f962f0cf6c9";
 export const metadata_url =
   "https://ctinsvafusekcbpznpfr.supabase.co/storage/v1/object/public/nft-metadata/metadata.json";
